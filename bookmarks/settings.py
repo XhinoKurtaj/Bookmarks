ff5d28f6a9395f2dffb3e4a0fd3957a18161ae06
xhinoKurtaj <xhino_kurtaj@yahoo.com>
2020-05-23 15:42:18 +0200

    Implemented edit user account with photo upload

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -123,3 +123,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
